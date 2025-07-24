a8f2d052b69faac0e6de483a8f76a6404cc6deba
Sergio Vegas Alonso <sergio.vegas.alonso@gmail.com>
2025-07-24 02:32:54 +0200

    server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import {
   AngularNodeAppEngine,
   createNodeRequestHandler,
-  isMainModule,
   writeResponseToNodeResponse,
 } from '@angular/ssr/node';
 import express from 'express';
@@ -51,16 +50,16 @@ app.use((req, res, next) => {
  * Start the server if this module is the main entry point.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
-if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
-    if (error) {
-      throw error;
-    }
-
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
-}
+// if (isMainModule(import.meta.url)) {
+//   const port = process.env['PORT'] || 4000;
+//   app.listen(port, (error) => {
+//     if (error) {
+//       throw error;
+//     }
+//
+//     console.log(`Node Express server listening on http://localhost:${port}`);
+//   });
+// }
 
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
