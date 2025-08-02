6f020f9fd0d9eaa1d71f96c2f911acd0dafb47c4
Sergio Vegas Alonso <sergio.vegas.alonso@gmail.com>
2025-08-02 17:21:33 +0200

    ok

--- src/app/shared/components/buttons/button/button.ts
+++ src/app/shared/components/buttons/button/button.ts
@@ -13,7 +13,7 @@ export class Button {
   configurationClasses = computed(() => {
     switch (this.buttonBehaviour()) {
       case ButtonBehaviour.Active:
-        return 'cursor-pointer bg-light-foreground text-light-foreground-contrast shadow-md hover:bg-light-foreground-highlighted dark:bg-dark-foreground dark:text-dark-foreground-contrast dark:hover:bg-dark-foreground-highlighted';
+        return 'cursor-pointer bg-light-foreground text-black shadow-md hover:bg-light-foreground-highlighted dark:bg-dark-foreground dark:text-white dark:hover:bg-dark-foreground-highlighted';
       case ButtonBehaviour.Disabled:
         return 'cursor-not-allowed opacity-50';
       default:
